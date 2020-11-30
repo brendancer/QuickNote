f53baff253465a11f3bdb782b0b21ff7ab6cfaf6
Brenda Goodwin <brendanced@gmail.com>
2020-11-30 16:16:32 -0700

    working out syntax problems

---
+++ routes/apiRoutes.js
@@ -0,0 +1,39 @@
+const noteData = require("../db/db.json");
+const uuid = require("uuid/v4");
+
+module.exports = function (app) {
+  app.get("api/notes", function (req, res) {
+    fs.readFile("../db/db.json", (err, data) => {
+      if (err) throw err;
+      const savedNotes = JSON.parse(data);
+      console.log(savedNotes);
+      res.send(savedNotes);
+    });
+  });
+
+  app.post("/api/notes", function (req, res) {
+    let noteData = JSON.parse(data);
+    let newNote = {
+      id: uuid,
+      title: req.body.title,
+      text: req.body.text,
+    };
+    noteData.push(newNote);
+    fs.writeFile("../db/db.json", JSON.stringify(noteData), (err) => {
+      if (err) throw err;
+      return res.json(noteData);
+    });
+  });
+
+  app.delete("./api/notes", function (req, res) {
+    fs.readFile("./db/db.json", function (err, data) {
+      if (err) throw error;
+      const upDatedArray = noteData.filter((item) => item.id != req.params.id);
+      console.log(upDatedArray);
+      fs.writeFile("./db/db.json", JSON.stringify(upDatedArray), function () {
+        console.log(`${item} has been deleted`);
+        return res.json(upDatedArray);
+      });
+    });
+  });
+};
--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const http = require("http");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const app = express();
+
 //Express Set up
+const app = express();
 var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-const apir = require("./routes/apiRoutes")(app);
-const htmlr = require("./routes/htmlRoutes")(app);
+//require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
   console.log(`app listening on PORT ${PORT}`);
