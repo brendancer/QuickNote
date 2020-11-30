4e6b28084e04af1f1f1f9f7c0b2b5cb5ce6b2184
Brenda Goodwin <brendanced@gmail.com>
2020-11-29 17:31:23 -0700

    server up and running

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+//Dependencies
 const http = require("http");
-const app = require("express");
-const path = require ("path");
-const fs = require ("fs");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+//Express Set up
+var PORT = 8080;
 
-var PORT = 8080
-
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/notes", function (req, res) {
+  console.log(`${req.method}: ${req.path} : ${JSON.stringify(rerq.params)}`);
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("/*", function (req, res) {
+  console.log(`${req.method}: ${req.path} : ${JSON.stringify(rerq.params)}`);
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, function () {
+  console.log(`app listening on PORT ${PORT}`);
+});
